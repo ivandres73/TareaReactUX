28d05d1fb502c49f5ba7283d62a9a850f25b0417
Ivan Carcamo <ivandres73@unitec.edu>
2018-03-03 12:11:50 -0800

    created first component

---
+++ public/js/app-1.js
@@ -0,0 +1,9 @@
+class ProductList extends React.Component {
+	render() {
+		return (
+		<div classNmae='ui unstackable items'>
+			Hello, firend! I am a basic React component.
+			</div>
+		);
+	}
+}
