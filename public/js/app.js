d836b30c4f15e1544e57c85d00a26c194df0ffba
Ivan Carcamo <ivandres73@unitec.edu>
2018-03-10 13:59:10 -0800

    Unificado a un solo archivo

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,28 @@
-import Product from './app-2';
 
 class ProductList extends React.Component {
 
-	constructor(props) {
-		super(props);
+	state = {
+		products: [],
+	};
 
+	/*constructor(props) {
+		super(props);
 		this.state = {
 			products: [],
 		};
-		this.handleUpVote = this.handleUpVote.bind(this);
-	}
+
+		this.handleProductUpVote = this.handleProductUpVote.bind(this);
+	}*/
 
 	componentDidMount() {
-		this.setState({ products: Seed.products});
+		this.setState({ products: Seed.products });
 	}
 
-	handleProductUpVote(productId) {
+	handleProductUpVote = (productId) => {
 		const nextProducts = this.state.products.map((product) => {
 			if (product.id === productId) {
 				return Object.assign({}, product, {
-					votes: product.votes + 1,
+				votes: product.votes + 1,
 				});
 			} else {
 				return product;
@@ -31,32 +34,84 @@ class ProductList extends React.Component {
 	}
 
   render() {
-  	return (
-  		<div className='ui unstackable items'>
+  	const products = this.state.products.sort((a, b) => (
+		b.votes - a.votes
+	));
 
-  			const products = this.state.products.sort((a, b) => (
-  				b.votes - a.votes
-  			))
-		  	const product = this.state.products.map(() => (
-		        <Product
-		        	key={'product-' + product.id}
-		        	id={product.id}
-		        	title={product.title}
-		        	description={product.description}
-		        	url={product.url}
-		        	votes={product.votes}
-		        	submitterAvatarUrl={product.submitterAvatarUrl}
-		        	productImageUrl={product.productImageUrl}
-		        	onVote={this.handleProductUpVote}
-		        />
-		    	));
-    	</div>
-    )
+  	/*const products = Seed.products.sort((a, b) => (
+		b.votes - a.votes
+	));*/
+
+	const productComponents = Seed.products.map((product) => (
+		<Product
+			key={'product-' + product.id}
+			id={product.id}
+			title={product.title}
+			description={product.description}
+			url={product.url}
+			votes={product.votes}
+			submitterAvatarUrl={product.submitterAvatarUrl}
+			productImageUrl={product.productImageUrl}
+			onVote={this.handleProductUpVote}
+		/>
+	));
+    return (
+      <div className='ui unstackable items'>
+        { productComponents }
+      </div>
+    );
   }
+}
+
+class Product extends React.Component {
+	/*constructor(props) {
+		super(props);
+		this.handleUpVote = this.handleUpVote.bind(this);
+	}
+
+	handleUpVote() {
+		this.props.onVote(this.props.id);
+	}*/
+
+	handleUpVote = () => (
+		this.props.onVote(this.props.id)
+	);
+
+	render() {
+		return (
+		<div className='item'>
+			<div className='image'>
+				<img src={this.props.productImageUrl} />
+			</div>
+				<div className='middle aligned content'>
+					<div className='header'>
+						<a onClick={this.handleUpVote}>
+							<i className='large caret up icon' />
+						</a>
+						{this.props.votes}
+					</div>
+					<div className='description'>
+						<a href={this.props.url}>
+							{this.props.title}
+						</a>
+						<p>
+							{this.props.description}
+						</p>
+					</div>
+					<div className='extra'>
+						<span>Submitted by:</span>
+						<img
+							className='ui avatar image'
+							src={this.props.submitterAvatarUrl}
+						/>
+					</div>
+			</div>
+		</div>
+		);
+	}
 }
 
 ReactDOM.render(
   <ProductList />,
   document.getElementById('content')
 );
-
